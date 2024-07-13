a93e15e956a67ee7c2465c3e946faa5846444a6c
chinese-wzq <2270638422@qq,com>
2024-07-14 06:11:06 +0800

    新增：支持Playtimes插件

---
+++ action/plugins/Playtimes.py
@@ -0,0 +1,14 @@
+from .. import base
+import os
+
+
+def process(server_info: base.ServerInfo):
+    data_file=os.path.join(server_info.server_path,"plugins","Playtimes","data.yml")
+    with open(data_file,"r") as f:
+        data=f.read()
+    # 删除包含new_uuid的行
+    data = '\n'.join([line for line in data.split('\n') if server_info.new_uuid not in line])
+    # 将所有old_uuid替换为new_uuid
+    data = data.replace(server_info.old_uuid, server_info.new_uuid)
+    with open(data_file,"w") as f:
+        f.write(data)
