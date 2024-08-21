c7005b2ca575200f78e03f0d8891baac852c2b56
chinese-wzq <2270638422@qq.com>
2024-08-21 03:25:27 +0000

    更新：现在可以单独迁移playerdata、成就、stats数据；更新了本地化文件

--- action/saves.py
+++ action/saves.py
@@ -3,60 +3,58 @@ from . import base
 
 
 def migrate_saves(server_info: base.ServerInfo):
-    if input(_("是否确认迁移玩家存档数据？(y/n)")) != "y":
-        return
     server_path = server_info.server_path
     old_uuid = server_info.old_uuid
     new_uuid = server_info.new_uuid
-    # 1. 找到玩家数据文件夹
-    player_data_path = os.path.join(server_path, "world", "playerdata")
-    if not os.path.exists(player_data_path):
-        print(_("未找到玩家数据文件夹"))
-        return
-    # 2. 找到玩家数据文件
-    player_data_files = os.listdir(player_data_path)
-    for file in player_data_files:
-        if file == old_uuid + ".dat":
-            # 3. 备份已存在的新UUID的玩家数据文件
-            base.backup_file(os.path.join(player_data_path, new_uuid + ".dat"), "playerdata", server_info)
-            # 4. 将旧UUID的玩家数据文件覆盖到新UUID的玩家数据文件
-            os.rename(os.path.join(player_data_path, old_uuid + ".dat"),
-                      os.path.join(player_data_path, new_uuid + ".dat"))
-            print(_("玩家数据迁移成功"))
-            break
+    if input(_("是否确认迁移playerdata？(y/n)")) == "y":
+        # 1. 找到玩家数据文件夹
+        player_data_path = os.path.join(server_path, "world", "playerdata")
+        # 2. 找到玩家数据文件
+        player_data_files = os.listdir(player_data_path)
+        if os.path.exists(os.path.join(player_data_path, old_uuid + ".dat")):
+            for file in player_data_files:
+                if file == old_uuid + ".dat":
+                    # 3. 备份已存在的新UUID的玩家数据文件
+                    base.backup_file(os.path.join(player_data_path, new_uuid + ".dat"), "playerdata", server_info)
+                    # 4. 将旧UUID的玩家数据文件覆盖到新UUID的玩家数据文件
+                    os.rename(os.path.join(player_data_path, old_uuid + ".dat"),
+                              os.path.join(player_data_path, new_uuid + ".dat"))
+                    print(_("playerdata迁移成功"))
+                    break
+        else:
+            print(_("需要迁移的playerdata不存在"))
     # =========================
-    # 1. 找到玩家成就数据文件夹
-    player_advancements_path = os.path.join(server_path, "world", "advancements")
-    if not os.path.exists(player_advancements_path):
-        print(_("未找到玩家成就数据文件夹"))
-        return
-    # 2. 找到玩家成就数据文件
-    player_advancements_files = os.listdir(player_advancements_path)
-    for file in player_advancements_files:
-        if file == old_uuid + ".json":
+    if input(_("是否确认迁移玩家成就数据？(y/n)")) == "y":
+        # 1. 找到玩家成就数据文件夹
+        player_advancements_path = os.path.join(server_path, "world", "advancements")
+        # 2. 找到玩家成就数据文件
+        player_advancements_files = os.listdir(player_advancements_path)
+        if os.path.exists(os.path.join(player_advancements_path, old_uuid + ".json")):
             # 3. 备份已存在的新UUID的玩家成就数据文件
             base.backup_file(os.path.join(player_advancements_path, new_uuid + ".json"), "advancements", server_info)
             # 4. 将旧UUID的玩家成就数据文件覆盖到新UUID的玩家成就数据文件
             os.rename(os.path.join(player_advancements_path, old_uuid + ".json"),
                       os.path.join(player_advancements_path, new_uuid + ".json"))
             print(_("玩家成就数据迁移成功"))
-            break
+        else:
+            print(_("需要迁移的玩家成就数据不存在"))
     # =========================
-    # 1. 找到状态数据文件夹
-    player_stats_path = os.path.join(server_path, "world", "stats")
-    if not os.path.exists(player_stats_path):
-        print(_("未找到状态数据文件夹"))
-        return
-    # 2. 找到状态数据文件
-    player_stats_files = os.listdir(player_stats_path)
-    for file in player_stats_files:
-        if file == old_uuid + ".json":
+    if input(_("是否确认迁移stats数据？(y/n)")) == "y":
+        # 1. 找到状态数据文件夹
+        player_stats_path = os.path.join(server_path, "world", "stats")
+        if not os.path.exists(player_stats_path):
+            print(_("未找到stats数据文件夹"))
+            return
+        # 2. 找到状态数据文件
+        player_stats_files = os.listdir(player_stats_path)
+        if os.path.exists(os.path.join(player_stats_path, old_uuid + ".json")):
             # 3. 备份已存在的新UUID的状态数据文件
             base.backup_file(os.path.join(player_stats_path, new_uuid + ".json"), "stats", server_info)
             # 4. 将旧UUID的状态数据文件覆盖到新UUID的状态数据文件
             os.rename(os.path.join(player_stats_path, old_uuid + ".json"),
                       os.path.join(player_stats_path, new_uuid + ".json"))
-            print(_("状态数据迁移成功"))
-            break
+            print(_("stats数据迁移成功"))
+        else:
+            print(_("需要迁移的stats数据不存在"))
     # =========================
     print(_("玩家存档数据已迁移完成"))
